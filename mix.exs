e57a1fc91315d6f38d5c4eb372f3c64fe1ee6775
Daniel Spofford <daniel.spofford@verypossible.com>
2020-04-03 19:30:11 -0500

    Release v0.2.1

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Hook.MixProject do
       package: package(),
       preferred_cli_env: [check: :test],
       start_permanent: Mix.env() == :prod,
-      version: "0.2.0"
+      version: "0.2.1"
     ]
   end
 
